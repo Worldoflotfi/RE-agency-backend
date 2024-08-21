695202193a89586d03ea83560d4242f48f64c8bd
Mohamed Amine Lotfi <medamineltf.100@gmail.com>
2024-08-21 14:02:36 +0100

    jwt.ts

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -121,6 +121,37 @@ export const activateUser = CatchAsyncError(async (req: Request, res: Response, 
     }
 });
 
+//login user 
+interface ILoginRequest {
+    email: string;
+    password: string;
+}
+
+export const loginUser = CatchAsyncError(async(req:Request,res:Response,next:NextFunction) => {
+    try{
+        const {email,password} = req.body as ILoginRequest;
+
+        if(!email || !password){
+            return next(new ErrorHandler('Please enter email and password',400));
+        };
+
+        const user = await userModel.findOne({email}).select('+password');
+
+        if(!user){
+            return next(new ErrorHandler("Invalid email or password",400));
+        };
+
+        const isPasswordMatch = await user.comparePassword(password);
+        if(!user){
+            return next(new ErrorHandler('Invalid email or password',400));
+        };
+    }
+    catch (error:any){
+        return next(new ErrorHandler(error.message, 400));
+    }
+})
+
+
 
 //check wrong OTP
 //print from pic ..
