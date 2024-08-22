43cd474836a44454e1913ad461bd2cc3873a845b
Mohamed Amine Lotfi <medamineltf.100@gmail.com>
2024-08-22 23:47:47 +0100

    ..

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -2,7 +2,7 @@
 import express from 'express';
 import { registrationUser, activateUser } from '../controllers/user.controller';
 
-const userRouter = express.Router(); 
+const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
 
