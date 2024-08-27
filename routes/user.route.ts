4ce236de2a963475b1fea71142a54a08944a8a68
Mohamed Amine Lotfi <medamineltf.100@gmail.com>
2024-08-27 15:03:59 +0100

    user login part 2

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,12 +1,14 @@
 //user route 
 import express from 'express';
-import { registrationUser, activateUser } from '../controllers/user.controller';
+import { registrationUser, activateUser, loginUser } from '../controllers/user.controller';
 
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
 
 userRouter.post('/activate-user', activateUser);
+
+userRouter.post('/login-user', loginUser);
 
 export default userRouter;
 
