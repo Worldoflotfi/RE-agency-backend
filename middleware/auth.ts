1ea965a9631585e328fefd35d288fb18c66dde3a
Mohamed Amine Lotfi <medamineltf.100@gmail.com>
2024-09-05 02:28:47 +0100

    protected route - init

---
+++ middleware/auth.ts
@@ -0,0 +1,14 @@
+import { Request,Response,NextFunction } from "express";
+import { CatchAsyncError } from "./catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)  => {
+    const access_token = req.cookies.access_token;
+
+    if(!access_token){
+        return next(new ErrorHandler("Please login to access this ressource",400));
+    }
+    
+    const decoded = jwt.verify(access_token,process.env.JWT_SECRET as string);
+})
