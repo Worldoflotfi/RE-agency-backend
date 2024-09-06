37edfd89cf3e141f09204fb479596b8858f4cc0a
Mohamed Amine Lotfi <medamineltf.100@gmail.com>
2024-09-06 23:53:40 +0100

    protected route - formatted

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,19 +1,19 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { CatchAsyncError } from "./catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import jwt from "jsonwebtoken";
 
-export const isAuthenticated = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)  => {
+export const isAuthenticated = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     const access_token = req.cookies.access_token;
 
-    if(!access_token){
-        return next(new ErrorHandler("Please login to access this ressource",400));
+    if (!access_token) {
+        return next(new ErrorHandler("Please login to access this ressource", 400));
     }
-    
-    const decoded = jwt.verify(access_token,process.env.ACCESS_TOKEN as string);
 
-    if(!decoded){
-        return next(new ErrorHandler("access token is not valid",400));
+    const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN as string);
+
+    if (!decoded) {
+        return next(new ErrorHandler("access token is not valid", 400));
     }
 
     //continue for protected route
